ff0cf1df57ca1bce36ec9aea09b986cd2604fbd6
Aswathi21 <aswathi.pv2@gmail.com>
2023-07-09 17:36:40 -0700

    client changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [data,setData] = useState('');
+  const [page, setPage] = useState(1);
+  
+  useEffect(()=>{
+    const options = {
+      method: 'GET',
+      headers: new Headers({ 'Content-type': 'application/json'})
+    }
+    fetch(`http://localhost:5000/${page}`,options).then(response => response.json()).then(data => setData(data));  
+  },[page]) 
+
+  const handleScroll = async() => {
+     if(window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight){
+        setPage((prev) => prev + 1);
+      } 
+  };
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Orkes UI</h1>
+      {data && data.map((item,key) => <div key={key} className='main-container'>
+        <div className='indivitual-container'>
+        <a href={item.path}>
+          <img className="image" src={item.field_photo_image_section} alt={item.title}/>
         </a>
-      </header>
+        <span className='title'>{item.title}</span>
+        </div>
+      </div>)}
     </div>
   );
 }
